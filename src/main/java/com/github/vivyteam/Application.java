f10cb41a2821a45f8c18781dbd040d6c8f17a37a
Mingxue Zhang <38911694+Mingxue-Zhang@users.noreply.github.com>
2022-09-30 15:25:49 +0200

    Update Application.java

--- src/main/java/com/github/vivyteam/Application.java
+++ src/main/java/com/github/vivyteam/Application.java
@@ -1,10 +1,68 @@
 package com.github.vivyteam;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Mono;
+
+import java.util.HashMap;
+import java.util.Random;
 
 @SpringBootApplication
 public class Application {
+
+    private HashMap<String, String> full_short = new HashMap<String, String>(); // key: full URL; value - shorted url
+
+    @GetMapping("/{urlToBeShortened}/short")
+    @ResponseBody
+    public  String toShortURL(@PathVariable("urlToBeShortened") String urlToBeShortened) { // shorten a URL
+        String getShortURL = mapShortURL(urlToBeShortened);
+        return "http://localhost:9000/"+getShortURL;
+    }
+
+    @GetMapping("/{shortenedUrl}/full") //short-to-full
+    @ResponseBody
+    public String getFullURL(@PathVariable("shortenedUrl") String shortenedUrl) {
+        for (Object key: full_short.keySet()) {
+            if (full_short.get(key).equals(shortenedUrl))
+                return key.toString();
+        }
+        return "Missing URL";
+    }
+
+    // redirect to full URL
+    @GetMapping("/shortenedUrl")
+    public String redirect(@PathVariable("shortenedUrl") String shortenedUrl) {
+        return ("redirect:/"+ getFullURL(shortenedUrl));
+        //return Renderer.redirectTo(getFullURL(shortenedUrl)).build();
+    }
+
+    // in case of IllegalArgumentException
+    @ExceptionHandler(IllegalArgumentException.class) //
+    public void illegalArgumentHandler() {
+        //
+    }
+
+    public String mapShortURL(String fullURL){
+        if (full_short.containsKey(fullURL)) {
+            return full_short.get(fullURL);
+        }
+        String shortURL = encodeURL(12);// Generate a 12-bit random string, or any length you want
+        full_short.put(fullURL, shortURL);
+        return shortURL;
+    }
+
+    public String encodeURL(int length){
+        String str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        Random random = new Random();
+        StringBuffer sb = new StringBuffer();
+        for(int i = 0; i < length; i++ ) {
+            int number=random.nextInt(62);
+            sb.append(str.charAt(number));
+        }
+        return sb.toString();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
